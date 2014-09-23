b1253a19015e463690a0aba431875d6235f7cec9
cdurth <durthacj@mail.uc.edu>
2014-09-23 00:06:38 -0400

    added labs/blog list and post views/controllers/styling

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = angular.module('app', ['ui.router']);
 
+var options = {};
+options.api = {};
+options.api.base_url = "http://vps2.plugnpromo.com:3333";
+
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/home');
     
@@ -31,10 +35,24 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
                     controller: 'navCtrl',
                 },
                 "content": {
-                    templateUrl: './views/labs.html',
+                    templateUrl: './views/post.list.html',
+                    controller: 'PostListCtrl',
                 }
             },
             url: '/labs',
+        })
+        .state('singlepost', {
+            views: {
+                "nav": {
+                    templateUrl: './views/nav.html',
+                    controller: 'navCtrl',
+                },
+                "content": {
+                    templateUrl: './views/post.view.html',
+                    controller: 'PostViewCtrl',
+                }
+            },
+            url: '/labs/:id',
         })
         .state('contact', {
             views: {
@@ -66,3 +84,163 @@ app.controller('mainCtrl',['$scope','$location',
         });
 }]);
 
+app.controller('PostListCtrl', ['$scope', '$sce', 'PostService',
+    function PostListCtrl($scope, $sce, PostService) {
+
+        $scope.posts = [];
+
+        PostService.findAllPublished().success(function(data) {
+            for (var postKey in data) {
+                data[postKey].content = $sce.trustAsHtml(data[postKey].content);
+            }
+
+            $scope.posts = data;            
+        }).error(function(data, status){
+            console.log(status);
+            console.log(data);
+        });
+    }
+]);
+
+app.controller('PostViewCtrl', ['$scope', '$stateParams', '$location', '$sce', 'PostService', 'LikeService',
+    function PostViewCtrl($scope, $stateParams, $location, $sce, PostService, LikeService){
+
+        $scope.post = {};
+        var id = $stateParams.id;
+
+        $scope.isAlreadyLiked = LikeService.isAlreadyLiked(id);
+
+        PostService.read(id).success(function(data) {
+            data.content = $sce.trustAsHtml(data.content);
+            $scope.post = data;
+        }).error(function(data, status){
+            console.log(status);
+            console.log(data);
+        });
+
+        //Like a post
+        $scope.likePost = function likePost(){
+            if (!LikeService.isAlreadyLiked(id)){
+                PostService.like(id).success(function(data) {
+                    $scope.post.likes++;
+                    LikeService.like(id);
+                    $scope.isAlreadyLiked = true;
+                }).error(function(data, status){
+                    console.log(status);
+                    console.log(data);
+                });
+            }
+        };
+
+        //Unlike a post
+        $scope.unlikePost = function unlikePost(){
+            if (LikeService.isAlreadyLiked(id)){
+                PostService.unlike(id).success(function(data){
+                    $scope.post.likes--;
+                    LikeService.unlike(id);
+                    $scope.isAlreadyLiked = false;
+                }).error(function(data, status){
+                    console.log(status);
+                    console.log(data);
+                });
+            }
+        }
+
+    }
+]);
+
+
+/*********************************************************************
+    SERVICES
+*********************************************************************/
+
+app.factory('PostService', function($http) {
+    return {
+        findAllPublished: function() {
+            return $http.get(options.api.base_url + '/post');
+        },
+
+        findByTag: function(tag) {
+            return $http.get(options.api.base_url + '/tag/' + tag);
+        },
+
+        read: function(id) {
+            return $http.get(options.api.base_url + '/post/' + id);
+        },
+        
+        findAll: function() {
+            return $http.get(options.api.base_url + '/post/all');
+        },
+
+        changePublishState: function(id, newPublishState) {
+            return $http.put(options.api.base_url + '/post', {'post': {_id: id, is_published: newPublishState}});
+        },
+
+        delete: function(id) {
+            return $http.delete(options.api.base_url + '/post/' + id);
+        },
+
+        create: function(post) {
+            return $http.post(options.api.base_url + '/post', {'post': post});
+        },
+
+        update: function(post) {
+            return $http.put(options.api.base_url + '/post', {'post': post});
+        },
+
+        like: function(id) {
+            return $http.post(options.api.base_url  + '/post/like', {'id': id});
+        },
+
+        unlike: function(id) {
+            return $http.post(options.api.base_url  + '/post/unlike', {'id': id});
+        }
+    };
+});
+
+
+app.factory('LikeService', function ($window) {
+    //Contains post ids already liked by the user
+    var postLiked = [];
+
+    if ($window.sessionStorage && $window.sessionStorage.postLiked) {
+        postLiked.push($window.sessionStorage.postLiked);
+    }
+
+
+    return {
+        isAlreadyLiked: function(id) {
+            if (id != null) {
+                for (var i in postLiked) {
+                    if (postLiked[i] == id) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return false;
+        },
+
+        like: function(id) {
+            if (!this.isAlreadyLiked(id)) {
+                postLiked.push(id);
+                $window.sessionStorage.postLiked = postLiked;
+            }
+        },
+
+        unlike: function(id) {
+            if (this.isAlreadyLiked(id)) {
+                for (var i in postLiked) {
+                    if (postLiked[i] == id) {
+                        postLiked.splice(i, 1);
+                        $window.sessionStorage.postLiked = postLiked;
+                        
+                        break;
+                    }
+                }
+            }
+        }
+    }
+});
